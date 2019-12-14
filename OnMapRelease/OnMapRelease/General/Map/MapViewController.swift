71f51c8e8b4ac58fb386990c8bd5e8e0bc6b71b6
serenehet <serenehet@gmail.com>
2019-12-14 21:24:24 +0300

    save changes

---
+++ OnMapRelease/OnMapRelease/General/Map/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  OnMapRelease
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //navigationController?.isToolbarHidden = true
+    }
+}
