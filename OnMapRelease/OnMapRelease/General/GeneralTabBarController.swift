a55d5c93aab285faddf078d1ad6ed0d7abf4c58e
serenehet <serenehet@gmail.com>
2019-12-14 21:41:57 +0300

    save changes

---
+++ OnMapRelease/OnMapRelease/General/GeneralTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  GeneralTabBarController.swift
+//  OnMapRelease
+//
+//  Created by Sergei Alexeev on 14.12.2019.
+//  Copyright © 2019 Sergei Alexeev. All rights reserved.
+//
+
+import UIKit
+
+class GeneralTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //self.tabBarController?.
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
