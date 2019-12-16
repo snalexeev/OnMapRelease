db4abca6bede0486c751c7245ece4846ba5753ae
serenehet <serenehet@gmail.com>
2019-12-16 23:04:18 +0300

    save changes

---
+++ onMap/onMap/Map/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  onMap
+//
+//  Created by Sergei Alexeev on 16.12.2019.
+//  Copyright © 2019 onMapTeam. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
