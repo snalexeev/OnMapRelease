d6aa8b8f35c1a231a2334a5f4a5fe390d357a17e
serenehet <serenehet@gmail.com>
2019-12-31 18:13:26 +0300

    save changes

---
+++ onMap/onMap/ViewControllers/ChatRoom/ChatRoomViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatRoomViewController.swift
+//  onMap
+//
+
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ onMap/onMap/ViewControllers/ChatRoom/MyMessageTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+
+
+import UIKit
+
+final class MyMessageTableViewCell: DiscussionMessageCell {
+    
+    @IBOutlet private weak var backView: UIView!
+    @IBOutlet private weak var textMessageLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupOutlets()
+
+        super.roundBackView()
+    }
+    
+    func setupOutlets() {
+        super.backMessageView = backView
+        super.messageLabel = textMessageLabel
+        super.timeSendLabel = timeLabel
+    }
+
+}
